9c6d66bd9641dbf26c5b7f3719418ed88cedf51b
CSAMU4 <samuelsmcandace@gmail.com>
2023-08-31 21:29:04 -0400

    created Cat class

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ * Coding Assignment Week 5 - Menu App 
+ * Create a menu app as seen in this week’s video. 
+ * What you create is up to you as long as it meets the following requirements:
+        Use at least one array.
+        Use at least two classes.
+        Your menu should have the options to create, view, and delete elements.
+ */
+
+class Cat {
+    constructor (name, breed) {
+        this.name = name; 
+        this.breed = breed; 
+    }
+}
